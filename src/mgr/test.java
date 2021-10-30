119f64a75f99a8836651382e251ed0a478888fd0
meenyoung <93370349+meenyoung@users.noreply.github.com>
2021-10-30 17:47:50 +0900

    [민영] 프린트 추가2323

--- src/mgr/test.java
+++ src/mgr/test.java
@@ -6,5 +6,7 @@ public class test {
         System.out.print("김수진");
         System.out.print("성유진");
         System.out.print("박민영");
+        System.out.print("박민영");
+        System.out.print("박민영");
     }
 }
