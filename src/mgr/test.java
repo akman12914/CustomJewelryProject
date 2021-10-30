1d5d609659be2e24e19233fcbe2b4e01592184a9
Yujin Sung <rnlduadns98@naver.com>
2021-10-30 17:51:36 +0900

    왜이래

--- src/mgr/test.java
+++ src/mgr/test.java
@@ -6,7 +6,7 @@ public class test {
         System.out.print("김수진");
         System.out.print("성유진");
         System.out.print("박민영");
-        System.out.print("박민영");
+        System.out.print("박민영ㅎ");
         System.out.print("박민영");
     }
 }
