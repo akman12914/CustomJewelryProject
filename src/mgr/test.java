5e3ab007267e455c134687a8fdbf6f74b0485904
Yujin Sung <rnlduadns98@naver.com>
2021-10-30 17:39:44 +0900

    초기화

--- src/mgr/test.java
+++ src/mgr/test.java
@@ -2,8 +2,6 @@ package mgr;
 
 public class test {
     public static void main(String[] args) {
-        System.out.print("되네");
-        System.out.print("김수진");
-        System.out.print("성유진");
+        System.out.print("초기화");
     }
 }
