05ea0028f634e4095aba5b861d7a8166fb66898f
Yujin Sung <rnlduadns98@naver.com>
2021-11-17 08:26:03 +0900

    [성유진] GUIMain 배경설정

--- src/jewely_demo/GUIMain.java
+++ src/jewely_demo/GUIMain.java
@@ -1,67 +1,48 @@
 package jewely_demo;
 
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JTable;
-import javax.swing.JToggleButton;
-import javax.swing.ListSelectionModel;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingConstants;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.table.DefaultTableModel;
 
 import store.JewelryStore;
-import store.Order;
-import store.OrderManager;
 
 import java.awt.BorderLayout;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 
 import javax.swing.*;
-import store.JewelryStore;
-import store.Order;
-import store.OrderManager;
 
 public class GUIMain extends JFrame{
     static JewelryStore store = JewelryStore.getInstance();
     private static final long serialVersionUID = 1L;
     //JTextField resultTF = new JTextField("0", 20);
-    JPanel topLabel = new JPanel(new GridLayout(1, 1));
-    JPanel middleLabel = new JPanel(new GridLayout(2, 10));
-    JPanel bottomLabel = new JPanel(new GridLayout(1, 1));
+    JScrollPane scrollPane;
+    ImageIcon icon = new ImageIcon("start.png");
+    JPanel background = new JPanel() {
+        public void paintComponent(Graphics g) {
+            g.drawImage(icon.getImage(), 0, 0, null);
+            setOpaque(false); //그림을 표시하게 설정,투명하게 조절
+            super.paintComponent(g);
+        }
+    };
     JButton detail = new JButton("주문내역 확인");
     JButton order = new JButton("주문하기");
 
     public void addComponentsTopane() {
         Container pane = getContentPane();
-        topLabel.setPreferredSize(new Dimension(300, 10));
-        middleLabel.setPreferredSize(new Dimension(300, 500));
-        bottomLabel.setPreferredSize(new Dimension(300, 150));
-        pane.add(topLabel, BorderLayout.PAGE_START);
-        pane.add(middleLabel, BorderLayout.CENTER);
-        pane.add(bottomLabel, BorderLayout.PAGE_END);
-
-        bottomLabel.add(detail, BorderLayout.WEST);
-        bottomLabel.add(order, BorderLayout.EAST);
-        ImageIcon Img = new ImageIcon("img.png");
-        JLabel imgLabel = new JLabel(Img);
-        middleLabel.add(imgLabel);
+        scrollPane = new JScrollPane(background);
+        setContentPane(scrollPane);
+        background.add(detail);
+        background.add(order);
 
         order.addActionListener(new ActionListener() {
             @Override
@@ -87,11 +68,6 @@ public class GUIMain extends JFrame{
         });
 
     }
-    /*public void paint(Graphics g) {//그리는 함수
-        g.drawImage(background, 0, 0, null);//background를 그려줌
-    }*/
-
-
     void createAndShowGUI() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         addComponentsTopane();
@@ -107,30 +83,41 @@ public class GUIMain extends JFrame{
 
     public class Ordered extends JFrame{
         Ordered() {
-            Container contentPane = this.getContentPane();
-            JPanel pane = new JPanel();
+            ImageIcon icon = new ImageIcon("start1.png");
+            JPanel background = new JPanel() {
+                public void paintComponent(Graphics g) {
+                    g.drawImage(icon.getImage(), 0, 0, null);
+                    setOpaque(false); //그림을 표시하게 설정,투명하게 조절
+                    super.paintComponent(g);
+                }
+            };
+            scrollPane = new JScrollPane(background);
+            setContentPane(scrollPane);
+            //Container contentPane = this.getContentPane();
+            //JPanel pane = new JPanel();
 
             JTextField textPeriod = new JTextField(5);
             JLabel labelPeriod = new JLabel("이름: ");
+            labelPeriod.setForeground(Color.white);
 
             JTextField textPeriod2 = new JTextField(5);
             JLabel labelPeriod2 = new JLabel("번호 : ");
+            labelPeriod2.setForeground(Color.white);
 
             JTextField textPeriod3 = new JTextField(5);
             JLabel labelPeriod3 = new JLabel("주소: ");
+            labelPeriod3.setForeground(Color.white);
 
 
             JButton buttonStart = new JButton("반지 색상 선택하기");
             buttonStart.setMnemonic('S');
-
-            pane.add(labelPeriod);
-            pane.add(textPeriod);
-            pane.add(labelPeriod2);
-            pane.add(textPeriod2);
-            pane.add(labelPeriod3);
-            pane.add(textPeriod3);
-            pane.add(buttonStart);
-            contentPane.add(pane);
+            background.add(labelPeriod);
+            background.add(textPeriod);
+            background.add(labelPeriod2);
+            background.add(textPeriod2);
+            background.add(labelPeriod3);
+            background.add(textPeriod3);
+            background.add(buttonStart);
 
             buttonStart.addActionListener(new ActionListener() {
                 @Override
@@ -174,9 +161,6 @@ public class GUIMain extends JFrame{
             pane.add(textPeriod2);
             pane.add(buttonStart);
             contentPane.add(pane);
-
-
-
         }
 
 
